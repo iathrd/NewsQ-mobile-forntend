f548e4872f9c53ba91dcc4c2b4d71469b2abc2e0
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-01-13 18:57:15 +0800

    created edit news

---
+++ src/screens/EditNews.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function EditNews() {
+  return (
+    <View>
+      <Text>Edit News</Text>
+    </View>
+  );
+}
